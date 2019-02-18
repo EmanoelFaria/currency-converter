f668cc039d4f1862c2422f7458156228b69877b3
Emanoel Faria <emanoel-faria@hotmail.com>
2019-02-18 19:55:07 -0300

    marcao inicial

---
+++ services/converter.ts
@@ -0,0 +1,10 @@
+class Converter {
+
+
+    //TODO: converte qualquer moeda para dolar
+    //TODO: converte dolar para qualquer moeda
+    //TODO: converte de moeda para outra moeda
+    //TODO: verificar valores de entrada
+}
+
+export { Converter }
