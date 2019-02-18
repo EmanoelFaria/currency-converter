7c6ff6033e6add239c0fd54e742869fcf1b87516
Emanoel Faria <emanoel-faria@hotmail.com>
2019-02-18 20:58:27 -0300

    removendo arquivos de locais inapropriados

--- app.js
+++
@@ -1,7 +0,0 @@
-const express           = require('express');
-const converterRouter   = require('./routes/converter');
-const app               = express();
-
-app.use('/converter', converterRouter);
-
-module.exports = app;
